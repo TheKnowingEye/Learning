49ce81fd2fa1b1001a082edad136cbc058dce464
TheKnowingEye <singhash030@gmail.com>
2025-07-26 14:30:08 +0530

    .

---
+++ Pytorch Book/Chapter 3/logistic.py
@@ -0,0 +1,32 @@
+import numpy as np
+class LogisticRegressionGD:
+    def __init__(self,eta,n_iter,random_state):
+        self.eta = eta
+        self.n_iter= n_iter
+        self.random_state = random_state
+
+    def fit(self,X,y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size= X.shape[1])
+        self.b_ = float(0.)
+
+        self.losses_ = []
+        
+        for _ in range(self.n_iter) :
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y-output)
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (-y.dot(np.log(output))-(1-y).dot(np.log(1-output))) / X.shape[0]
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self,X):
+        return np.dot(X, self.w_)+self.b_
+    
+    def activation(self, x):
+        return 1. / (1.+np.exp(-x))
+    
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X))>=0.5, 1 , 0)
